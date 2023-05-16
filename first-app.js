054b1fd68cfda82854f5b3d3e07194d16dfa37fb
gautamankita123 <ankitagautam6263@gmail.com>
2023-05-16 13:40:13 +0530

    first commit

--- first-app.js
+++ first-app.js
@@ -1,18 +1,27 @@
-const http = require('http');
-
 const express = require('express');
+const bodyparser = require('body-parser');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next();//allow the request to continue to the next middleware in line
+app.use(bodyparser.urlencoded({ extended: false }));
+
+
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
-app.use((req, res, next) => {
+
+app.post('/product', (req, res, next) => {
+    console.log(req, body);
+    res.redirect('/');
+
+});
+
+app.use('/', (req, res, next) => {
+
     console.log('In another middleware!');
     res.send('<h1>Hello from Express!<h1>');
 });
 
-const server = http.createServer(app);
 
 
-server.listen(3000);
+app.listen(3000);
